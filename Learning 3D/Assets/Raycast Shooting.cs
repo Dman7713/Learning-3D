6bdc902257000dc7275e4f88c29e7c0f3b8876ba
Dman7713 <PeytonPrivratsky@gmail.com>
2025-02-06 08:05:26 -0800

    update

---
+++ Learning 3D/Assets/Raycast Shooting.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class RaycastShooting : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        if(Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+}
